0e1a0c77851f0988655ad19326ab73b192185b37
tendirustendi <42054274+tendirustendi@users.noreply.github.com>
2018-11-19 16:07:09 +0700

    Update nlpLine.java

--- src/main/java/com/telkom/nlp/webhooks/nlpLine.java
+++ src/main/java/com/telkom/nlp/webhooks/nlpLine.java
@@ -18,6 +18,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import java.util.Random;
 
 /**
  *
