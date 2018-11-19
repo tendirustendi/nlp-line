e1d10e6bb43f617598f79b4bdc87630d0296ba23
Tendi Rustandi <rustanditendz@gmail.com>
2018-11-19 14:11:59 +0700

    Initial commit

---
+++ src/main/java/com/telkom/nlp/webhooks/nlpLine.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.telkom.nlp.webhooks;
+
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import java.util.concurrent.ExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ *
+ * @author buzdevbigdata
+ */
+@SpringBootApplication
+@LineMessageHandler
+public class nlpLine extends SpringBootServletInitializer {
+
+    @Autowired
+    private LineMessagingClient lineMessagingClient;
+
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(nlpLine.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(nlpLine.class, args);
+    }
+    
+    @EventMapping
+    public void handleTextEvent(MessageEvent<TextMessageContent> messageEvent){
+        String pesan = messageEvent.getMessage().getText().toLowerCase();
+        String[] pesanSplit = pesan.split(" ");
+        if(pesanSplit[0].equals("apakah")){
+//            String jawaban = getRandomJawaban();
+//            String replyToken = messageEvent.getReplyToken();
+//            balasChatDenganRandomJawaban(replyToken, jawaban);
+        }
+    }
+    
+    private void balasChatDenganRandomJawaban(String replyToken, String jawaban){
+        TextMessage jawabanDalamBentukTextMessage = new TextMessage(jawaban);
+        try {
+            lineMessagingClient
+                    .replyMessage(new ReplyMessage(replyToken, jawabanDalamBentukTextMessage))
+                    .get();
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println("Ada error saat ingin membalas chat");
+        }
+    }
+
+}
